ab11d39c70072a091e7a31004699dc0c13bab121
Alex Keil <alexpkeil1@users.noreply.github.com>
2023-08-31 00:58:46 -0400

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,22 +4,25 @@ using Documenter
 DocMeta.setdocmeta!(LSurvival, :DocTestSetup, :(using LSurvival); recursive=true)
 
 makedocs(;
-    modules=[LSurvival],
-    authors="Alex Keil <alex.keil@nih.gov>",
-    repo="https://github.com/alexpkeil1/LSurvival.jl/blob/{commit}{path}#{line}",
-    sitename="LSurvival.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://alexpkeil1.github.io/LSurvival.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+#   modules=[LSurvival],
+#   authors="Alex Keil <alex.keil@nih.gov>",
+#   repo="https://github.com/alexpkeil1/LSurvival.jl/blob/{commit}{path}#{line}",
+#   sitename="LSurvival.jl",
+#   format=Documenter.HTML(;
+#       prettyurls=get(ENV, "CI", "false") == "true",
+#       canonical="https://alexpkeil1.github.io/LSurvival.jl",
+#       edit_link="main",
+#       assets=String[],
+#   ),
+#   pages=[
+#       "Overview" => "index.md",
+#   #"Tutorial" => [
+#   #    "tutorial/introduction.md",
+#   #    "Advanced" => "tutorial/features.md",
+#   #],    
+#   ],
 )
 
 deploydocs(;
-    repo="github.com/alexpkeil1/LSurvival.jl",
-    devbranch="main",
+    repo="github.com/alexpkeil1/LSurvival.jl"
 )
