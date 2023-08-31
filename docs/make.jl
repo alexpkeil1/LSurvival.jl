38b35897ef4a653e34e00eb2e36251964f8861fe
Alex Keil <alexpkeil1@users.noreply.github.com>
2023-08-31 14:06:37 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,19 +7,19 @@ makedocs(;
    authors="Alex Keil <alex.keil@nih.gov>",
    repo="https://github.com/alexpkeil1/LSurvival.jl.git",
    sitename="LSurvival.jl",
-#   format=Documenter.HTML(;
-#       prettyurls=get(ENV, "CI", "false") == "true",
-#       canonical="https://alexpkeil1.github.io/LSurvival.jl",
-#       edit_link="main",
-#       assets=String[],
-#   ),
-#   pages=[
-#       "Overview" => "index.md",
+   format=Documenter.HTML(;
+       prettyurls=get(ENV, "CI", "false") == "true",
+       canonical="https://alexpkeil1.github.io/LSurvival.jl",
+       edit_link="main",
+       assets=String[],
+   ),
+   pages=[
+       "Overview" => "index.md",
 #   #"Tutorial" => [
 #   #    "tutorial/introduction.md",
 #   #    "Advanced" => "tutorial/features.md",
 #   #],    
-#   ],
+   ],
 )
 
 deploydocs(;
